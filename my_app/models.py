8a53b6cbe398198f55d751219402033790821ee5
osman-goni-cse <osmancse32@gmail.com>
2022-08-22 21:31:24 +0600

    add parameters in custom validators.py

--- my_app/models.py
+++ my_app/models.py
@@ -8,17 +8,17 @@ from .validators import validate_file_extension, validate_file_size, validate_im
 
 class Image(models.Model):
   text = models.TextField(max_length=1000, default="What's up")
-  photo = models.ImageField(upload_to="myimage", validators=[validate_file_size, validate_file_extension, validate_image_dimension])
+  photo = models.ImageField(upload_to="myimage", validators=[validate_file_size, validate_file_extension, validate_image_dimension(150, 150)])
   date = models.DateTimeField(null=True)
   event = models.DateTimeField(null=True)
 
-  def save(self): # new
-    super().save()
-    img = Im.open(self.photo.path)
-    print(img.height)
-    # resize it
-    if img.height > 300 or img.width > 300:
-        output_size = (300,300)
-        img.thumbnail(output_size)
-        img.save(self.photo.path)
+  # def save(self): 
+  #   super().save()
+  #   img = Im.open(self.photo.path)
+  #   print(img.height)
+  #   # resize it
+  #   if img.height > 300 or img.width > 300:
+  #       output_size = (300,300)
+  #       img.thumbnail(output_size)
+  #       img.save(self.photo.path)
   
--- my_app/validators.py
+++ my_app/validators.py
@@ -2,12 +2,20 @@ from django.core.exceptions import ValidationError
 from PIL import Image as Im
 from django.core.files.images import get_image_dimensions
 
-def validate_image_dimension(value):
+def validate_image_dimension(width, height):
+    def check_fun(value):
+        img_width, img_height = get_image_dimensions(value)
   
-  img_width, img_height = get_image_dimensions(value)
+        if img_height != height or img_width != width:
+            raise ValidationError(f'Image must be {width}px X {height}px')
+    return check_fun
+
+# def validate_image_dimension(value):
   
-  if img_height != 300 or img_width != 300:
-    raise ValidationError('Image must be 300px X 300px')
+#   img_width, img_height = get_image_dimensions(value)
+  
+#   if img_height != 100 or img_width != 100:
+#     raise ValidationError(f'Image must be 100px X 100px')
 
 def validate_file_extension(value):
     import os
